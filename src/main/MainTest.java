4cfee673d6ebb836542e3b5ba131c81357611407
Link-yu <kevinyulk@163.com>
2019-10-09 00:22:41 +0800

    递归法修改成一次hash法

--- src/main/MainTest.java
+++ src/main/MainTest.java
@@ -1,5 +1,9 @@
 package main;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created on 2017/7/17.
  * Title: Simple
@@ -10,7 +14,27 @@ package main;
  * @author 维斯
  */
 public class MainTest {
+    int[] result = new int[2];
     public static void main(String[] args) {
+        MainTest solution = new MainTest();
+        int[] nums = {2, 7, 11, 15};
+        System.out.println(solution.twoSum2(nums,9));
+    }
+
+    //
+    public int[] twoSum2(int[] nums, int target) {
+        Arrays.sort(nums);
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length;i++) {
+            int temp = target - nums[i];
+            if (map.containsKey(temp)) {
+                return new int[] {map.get(temp), i};
+            } else {
+                map.put(nums[i],i);
+            }
+        }
+        return result;
 
     }
+
 }
