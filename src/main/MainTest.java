08d7a9eb21d86dec6e0193c918ba45a8b41f7272
yulk <yulk@ibuscloud.com>
2019-10-10 15:37:01 +0800

    【10.10】【第4题 寻找两个有序数组的中位数】【更新满足时间复杂度O(log(m+n))的算法(二分法)】

--- src/main/MainTest.java
+++ src/main/MainTest.java
@@ -17,53 +17,18 @@ public class MainTest {
         MainTest solution = new MainTest();
         int[] nums1 = {1,2,7};
         int[] nums2 = {3,4};
-        System.out.println(solution.findMedianSortedArrays(nums1, nums2));
+        System.out.println(solution.longestPalindrome());
     }
 
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int len1 = nums1.length;
-        int len2 = nums2.length;
-        if (len1 > len2) {
-            return findMedianSortedArrays(nums2, nums1);
-        }
-        int iMin = 0;
-        int iMax = len1;
-        while (iMin < iMax) {
-            int i = (iMin + iMax)/2;
-            int j = (len1+len2+1)/2-i;
+    public String longestPalindrome(String s) {
+        int max = 0;
+        for (int i = 0; i < s.length();i++) {
+            for (int j = i+1; j < s.length();j++) {
 
-            if (i < iMax && nums2[j-1] > nums1[i]) {
-                iMin = iMin + 1;
-            } else if (i > iMin && nums1[i-1] > nums2[j]) {
-                iMax = iMax - 1;
-            } else {
-                int maxLeft = 0;
-                if (i == 0) {
-                    maxLeft = nums2[j-1];
-                } else if (j == 0) {
-                    maxLeft = nums1[i-1];
-                } else {
-                    maxLeft = Math.max(nums1[i-1], nums2[j-1]);
-                }
-
-                if ((len1 + len2) % 2 ==1) {
-                    return maxLeft;
-                }
-
-                int minRight = 0;
-                if (i == len1) {
-                    minRight = nums2[j];
-                } else if (j == len2) {
-                    minRight = nums1[i];
-                } else {
-                    minRight = Math.min(nums1[i], nums2[j]);
-                }
-
-                return (minRight + maxLeft)/2.0;
             }
         }
-        return 0.0;
     }
+
 
 }
 class ListNode {
