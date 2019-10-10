f84b093ce76d6d0736691f840ee6cfb4172f528f
yulk <yulk@ibuscloud.com>
2019-10-10 17:46:43 +0800

    【10.10】【第4题 寻找两个有序数组的中位数】【更新满足时间复杂度O(log(m+n))的算法(二分法)】

--- src/main/MainTest.java
+++ src/main/MainTest.java
@@ -17,16 +17,57 @@ public class MainTest {
         MainTest solution = new MainTest();
         int[] nums1 = {1,2,7};
         int[] nums2 = {3,4};
-        System.out.println(solution.longestPalindrome());
+        System.out.println(solution.longestPalindrome(""));
     }
 
     public String longestPalindrome(String s) {
-        int max = 0;
-        for (int i = 0; i < s.length();i++) {
-            for (int j = i+1; j < s.length();j++) {
+        if (s.length() == 1) {
+            return s;
+        }
+        if(s.length()<0 || s.equals(""))
+            return "";
 
+
+        int start = 0;
+        int end = 0;
+        String result = "";
+        for (int i = 0; i < s.length();i++) {
+            int len1 = aroundCenter(s, i,i);
+            int len2 = aroundCenter(s, i, i+1);
+
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+
+        }
+
+        return s.substring(start, end+1);
+    }
+
+    public int aroundCenter(String s, int left, int right) {
+        while (left >=0 && right<s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+
+
+
+    public boolean isHuiwen(String s) {
+        int start = 0;
+        int end = s.length()-1;
+
+        while (start <= end) {
+            if (s.charAt(start++) == s.charAt(end--)) {
+                continue;
+            } else {
+                return false;
             }
         }
+        return true;
     }
 
 
@@ -34,5 +75,5 @@ public class MainTest {
 class ListNode {
     int val;
     ListNode next;
-    ListNode(int x) { val = x; }
+    ListNode(int x) { val = x;}
 }
