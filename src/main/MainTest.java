846f72f810c5f958768e385397e5fccf6ea2065d
Link-yu <kevinyulk@163.com>
2019-10-22 23:45:56 +0800

    【10.20】【第23题】

--- src/main/MainTest.java
+++ src/main/MainTest.java
@@ -31,34 +31,49 @@ public class MainTest {
 //        node.next.next.next.next = new ListNode(5);
         List<String> list = new ArrayList<>();
 //        solution.quan(nums2,0, "", list);
-        solution.generateParenthesis(1);
+        ListNode[] lists = {l1, l2};
+        System.out.println(solution.mergeKLists(lists));
         System.out.println(strs[0].substring(0, strs[0].length()-1));
     }
 
-    public List<String> generateParenthesis(int n) {
-        if(n<=0){
-            return null;
-        }
-        ArrayList<String>	list = new ArrayList<String>();
-        String str = new String();
-        dfs(list,str,n,n);
-        return list;
+    public ListNode mergeKLists(ListNode[] lists) {
+        ListNode result = null;
+
+        return merge(lists,result,0);
+
     }
 
-    private void dfs(ArrayList<String> list, String str, int left, int right){
-        if(left>right)	{
-            return;//problem with ")("
+    public ListNode merge(ListNode[] lists, ListNode temp, int index) {
+        if (index == lists.length) {
+            return temp;
         }
-        if(left==0&&right==0){
-            list.add(str);
-            return;
+        temp = mergeTwoLists(lists[index], temp);
+        return merge(lists, temp, index+1);
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode result = new ListNode(0);
+        ListNode tmp = result;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                tmp.next = l1;
+                l1 = l1.next;
+            } else {
+                tmp.next = l2;;
+                l2 = l2.next;
+            }
+            tmp = tmp.next;
         }
-        if(left>0){
-            dfs(list,str+"(",left-1,right);
+        if (l1 != null) {
+            tmp.next = l1;
         }
-        if(right>0){
-            dfs(list, str+")",left,right-1);
+
+        if (l2 != null) {
+            tmp.next = l2;
         }
+
+        return result.next;
     }
 
 }
