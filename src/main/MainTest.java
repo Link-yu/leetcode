a8dcb4908fb7bbd7fc6e4e5a62f6e411753d945b
yulk <yulk@ibuscloud.com>
2019-11-27 09:17:51 +0800

    【11月27号】【第41题】

--- src/main/MainTest.java
+++ src/main/MainTest.java
@@ -21,39 +21,35 @@ import java.util.concurrent.LinkedBlockingQueue;
 public class MainTest {
     List<List<Integer>> listList = new ArrayList<>();
     public static void main(String[] args) {
-        int[] words = {10,1,2,7,6,1,5};
+        int[] words = {3,4,-1,1};
         ListNode l = new ListNode(1);
         l.next = new ListNode(2);
 //        l.next.next = new ListNode(3);
 //        l.next.next.next = new ListNode(4);
 //        l.next.next.next.next = new ListNode(5);
         MainTest m = new MainTest();
-        System.out.println(m.combinationSum2(words,8));
-    }
-    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        Arrays.sort(candidates);
-        List<Integer> list = new ArrayList<>();
-        helper(candidates,target, 0, list);
-        return listList;
+        System.out.println(m.firstMissingPositive(words));
     }
 
-    public void helper(int[] candidates, int target, int start, List<Integer> list) {
-        if (target == 0) {
-            listList.add(new ArrayList<>(list));
-            return;
-        }
-        for(int i = start; i <  candidates.length;i++) {
-            if (target < candidates[i]) {
-                break;
-            }
-            List<Integer> lists = new ArrayList<>(list);
-            list.add(candidates[i]);
-            helper(candidates, target - candidates[i], i+1, list);
-            list = lists;
-            while(i+1 < candidates.length && candidates[i+1] == candidates[i]) {
+    public int firstMissingPositive(int[] nums) {
+        for(int i=0;i<nums.length;){
+            if(nums[i]>0&&nums[i]<=nums.length&&nums[i]!=nums[nums[i]-1]){
+                //确定nums[i]的值对应的下标不越界，同时排除num[i]本身位置正确或者nums[i]应该放入的位置nums[i]-1原本就是nums[i](如[1,1])
+
+                int index = nums[i];//
+                nums[i] = nums[index -1];
+                nums[index -1]=index;
+                //换位置之后需要继续判断换过来的值是否在对的位置上，因此不能i++;
+            }else{
                 i++;
             }
         }
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=i+1){
+                return i+1;
+            }
+        }
+        return nums.length+1;
     }
 
 }
