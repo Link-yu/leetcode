23652336e13d1485c8cf79d36ab07c4f36bc4220
yulk <yulk@ibuscloud.com>
2020-08-14 13:56:18 +0800

    8月14号

--- src/main/MainTest.java
+++ src/main/MainTest.java
@@ -17,7 +17,46 @@ import java.util.*;
 public class MainTest {
     public static void main(String[] args) {
         String [] cityCodes = "361100".split(",");
-        System.out.println(cityCodes[0]);
+        MainTest mainTest = new MainTest();
+        System.out.println(mainTest.addBinary("1010","1011"));
+    }
+
+    public String addBinary(String a, String b) {
+        int lenA = a.length()-1;
+        int lenB = b.length()-1;
+        int temp = 0;
+        String result = "";
+        int atemp = 0;
+        int btemp =0 ;
+        while (lenA >= 0 || lenB >= 0) {
+            if (lenA >= 0) {
+                atemp = Integer.valueOf(a.charAt(lenA) + "");
+            }
+
+            if (lenB >= 0) {
+                btemp = Integer.valueOf(b.charAt(lenB) + "");
+            }
+            int plus = btemp + atemp + temp;
+            if (plus <= 1) {
+                result = plus + result;
+                temp = 0;
+            } else if (plus == 2) {
+                result = 0 + result;
+                temp = 1;
+            } else {
+                result = 1 + result;
+                temp = 1;
+            }
+            lenA--;
+            lenB--;
+            atemp = 0;
+            btemp = 0;
+        }
+        if (temp == 1) {
+            result = 1 + result;
+        }
+
+        return result;
     }
 
     public boolean hasGroupsSizeX(int[] deck) {
