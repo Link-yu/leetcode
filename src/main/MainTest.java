1aed3d62cf1c4260865805e67aa8c8b2a6202276
yulk <yulk@ibuscloud.com>
2020-08-18 08:01:27 +0800

    8月18号

--- src/main/MainTest.java
+++ src/main/MainTest.java
@@ -18,7 +18,50 @@ public class MainTest {
     public static void main(String[] args) {
         String [] cityCodes = "361100".split(",");
         MainTest mainTest = new MainTest();
-        System.out.println(mainTest.addBinary("1010","1011"));
+
+        System.out.println(mainTest.climbStairs(45));
+    }
+
+    public int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+
+        return climbStairs(n-1) + climbStairs(n-2) ;
+    }
+
+    public int climbStairs2(int n) {
+        int p = 0, q = 0, r = 1;
+        for (int i = 1; i <= n; ++i) {
+            p = q;
+            q = r;
+            r = p + q;
+        }
+        return r;
+    }
+
+    public int mySqrt(int x) {
+        if (x == 0) {
+            return 0;
+        }
+        Long right = Long.valueOf(x);
+        Long left = 1L;
+        Long mid = (right +left)/2;
+        while (right -left > 1) {
+            Long temp = mid*mid;
+            if (temp > x) {
+                right = mid;
+            } else if(temp < x) {
+                left = mid;
+            } else {
+                return mid.intValue();
+            }
+            mid = (right + left) /2;
+        }
+        return left.intValue();
     }
 
     public String addBinary(String a, String b) {
